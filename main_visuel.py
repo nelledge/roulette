706c8cfc825f238df93d5f89c84efff0685c6a82
Noah <noah@elledge.me>
2025-11-18 09:21:48 +0100

    .

--- main_visuel.py
+++ main_visuel.py
@@ -4,14 +4,14 @@ pygame.init()
 
 screen = pygame.display.set_mode((1, 1))  
 
-background = pygame.image.load("pictures/roulett.jpg").convert_alpha()
+background = pygame.image.load("pictures/roulette.jpg").convert_alpha()
 background = pygame.transform.scale(background, 
                                    (background.get_width()/4,
                                     background.get_height()/4))
 
 img_w = background.get_width()
 img_h = background.get_height()
-screen = pygame.display.set_mode((img_w, img_h))
+screen = pygame.display.set_mode((img_w+ 400, img_h + 100))
 
 def main():
     running = True
